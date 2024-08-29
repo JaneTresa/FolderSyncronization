3016f1907b999f4ab78333ad69b4d2bf206b4fb2
Jane Tresa <janetresa14@gmail.com>
2024-08-29 17:33:45 +0200

    Initial commit

---
+++ FolderSyncronization/FolderSyncronization.cs
@@ -0,0 +1,93 @@
+﻿namespace FolderSyncronization_Veeam
+{
+    public class FolderSyncronization
+    {
+        /// <summary>
+        /// Syncronize th source directory to the replica directory
+        /// </summary>
+        /// <param name="sourceDirectory"></param>
+        /// <param name="replicaDirectory"></param>
+        /// <exception cref="Exception"></exception>
+        public static void SyncronizeFolders(string sourceDirectory, string replicaDirectory)
+        {
+            try
+            {
+                //check if the directories exist
+                if (!Directory.Exists(sourceDirectory))
+                {
+                    throw new DirectoryNotFoundException($"Source directory not found: {sourceDirectory}");
+                }
+
+                if (!Directory.Exists(replicaDirectory))
+                {
+                    Directory.CreateDirectory(replicaDirectory);
+                }
+
+                Console.WriteLine($"Syncronizing {sourceDirectory} to {replicaDirectory}");
+                Program.Log("Syncronizing" +sourceDirectory+ "to" +replicaDirectory);
+
+                //copy files from source to replica after validation
+                foreach (string files in Directory.GetFiles(sourceDirectory))
+                {
+                    var sourceFile = Path.GetFileName(files);
+                    var replicaFileDir = Path.Combine(replicaDirectory, sourceFile);
+
+                    if (!File.Exists(replicaFileDir) || File.GetLastAccessTimeUtc(replicaFileDir) < File.GetLastAccessTimeUtc(sourceFile))
+                    {
+                        File.Copy(files, replicaFileDir, true);
+                        Console.WriteLine("Copying files from source to replica folder");
+                        Program.Log("Copying files from source to replica folder");
+                    }
+
+                }
+
+                //Delete the files that are not present in the source directory
+                foreach (string files in Directory.GetFiles(replicaDirectory))
+                {
+                    var replicaFile = Path.GetFileName(files);
+                    var sourceFileDir = Path.Combine(sourceDirectory, replicaFile);
+
+                    if (!File.Exists(sourceFileDir))
+                    {
+                        File.Delete(files);
+                        Console.WriteLine("Deleted outdated files from replica folder");
+                        Program.Log("Deleting outdated files from replica folder");
+                    }
+                }
+
+                //Delete outdated directories in the replica
+                foreach (string directory in Directory.GetDirectories(replicaDirectory))
+                {
+                    var replicaFileName = Path.GetDirectoryName(directory);
+                    var sourceDir = Path.Combine(sourceDirectory, replicaFileName);
+
+                    if (!File.Exists(sourceDir))
+                    {
+                        Directory.Delete(directory, true);
+                        Console.WriteLine("Deleted outdated directories from replica");
+                        Program.Log("Deleted outdated directories from replica");
+                    }
+                }
+
+                //copy sub folders from source to replica
+                string[] sourceSubDirectories = Directory.GetDirectories(sourceDirectory);
+                foreach (var sourceSubDir in sourceSubDirectories)
+                {
+                    string subDirName = Path.GetFileName(sourceSubDir);
+                    string destSubDir = Path.Combine(replicaDirectory, subDirName);
+
+                    ThreadPool.QueueUserWorkItem(state => SyncronizeFolders(sourceSubDir, destSubDir));
+                }
+
+                Console.WriteLine("Syncronized directories with source");
+                Program.Log("Syncronization completed at " + DateTime.UtcNow);
+
+            }
+            catch (Exception e)
+            {
+                Program.Log("Error: " + e.Message);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ FolderSyncronization/Program.cs
@@ -0,0 +1,105 @@
+﻿using FolderSyncronization_Veeam;
+using System.Diagnostics;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+class Program
+{
+    public static string _sourcePath = string.Empty;
+    public static string _replicaPath = string.Empty;
+    public static Timer _timer = new();
+    public static int _interval;
+    public static string _logFilePath = string.Empty;
+    public static object _lock = new();
+    public static void Main(string[] args)
+    {
+        try
+        {
+            //get source, destination path and interval frequency from user
+            Console.WriteLine("Enter the source directory");
+            _sourcePath = Console.ReadLine();
+            _sourcePath = string.IsNullOrEmpty(_sourcePath) ? throw new ArgumentNullException("Source path is empty") : _sourcePath;
+            Console.WriteLine("Enter the replica directory");
+            _replicaPath = Console.ReadLine();
+            _replicaPath = string.IsNullOrEmpty(_replicaPath) ? throw new ArgumentNullException("Replica path is empty") : _replicaPath;
+            if (_replicaPath.Equals(_sourcePath))
+            {
+                Console.WriteLine("Source and replica's path should be different");
+                throw new Exception("Source and replica's path should be different");
+            }
+            Console.WriteLine("Your desired interval for syncing folders? (mins)");
+            _interval = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the logs path");
+            _logFilePath = Console.ReadLine();
+            _logFilePath = string.IsNullOrEmpty(_logFilePath) ? throw new ArgumentNullException("Logs path is empty") : _logFilePath;
+            SetTimer(_interval);
+        }
+        catch(ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch(Exception e)
+        {
+            Log("Error: " + e.Message);
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+
+    /// <summary>
+    /// Set timer to trigger events periodically
+    /// </summary>
+    /// <param name="interval"></param>
+    public static void SetTimer(int interval)
+    {
+        _timer.Interval = interval * 60000;
+        _timer.Elapsed += OnTimedEvent;
+        _timer.Enabled = true;
+        _timer.AutoReset = true;
+        Log("Event triggered at " + DateTime.UtcNow + " press enter to exit");
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Trigger syncronization for folders and it's directories
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <param name="args"></param>
+    public static void OnTimedEvent(object origin, ElapsedEventArgs args)
+    {
+        Log("Syncronization Started at " + args.SignalTime.ToUniversalTime());
+        ThreadPool.QueueUserWorkItem(state => FolderSyncronization.SyncronizeFolders(_sourcePath, _replicaPath)); 
+    }
+
+    /// <summary>
+    /// Log events and errors if any in specified log path
+    /// </summary>
+    /// <param name="message"></param>
+    public static void Log(string message)
+    {
+        lock(_lock)
+        {
+            try
+            {
+                if (!File.Exists(_logFilePath))
+                {
+                    using (StreamWriter sw = File.CreateText(_logFilePath))
+                    {
+                        sw.WriteLine(message);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = File.AppendText(_logFilePath))
+                    {
+                        sw.WriteLine(message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to log file: {ex.Message}");
+            }
+        }
+    }
+}
